542fc5b524ae564cbd0fed17ea8a6f49befe7449
Doron Hafner <dh9707@gmail.com>
2020-10-04 19:26:24 +0300

    finish exercises and extension 1

--- src/components/Fentities.js
+++ src/components/Fentities.js
@@ -4,16 +4,20 @@ import '../styles/fentity-directory.css'
 
 class Fentities extends Component {
     render() {
+        const fentitiesCat = this.props.match.params.fentities
+        const fentities = this.props.state[fentitiesCat]
         return (
             <div>
-                <h1 id="fentities-title">{/*Get from `match`*/}</h1>
+                <h1 id="fentities-title">{fentitiesCat}</h1>
                 <div id="fentities-container">
                     {fentities.map(f => {
                         return (
-                            <div className="mini">
-                                <img className="directory-img" src={f.imgUrl} alt="" />
-                                <span>{f.name}</span>
-                            </div>
+                            <Link to={`/directory/${fentitiesCat}/${f.name}`}>
+                                <div className="mini">
+                                    <img className="directory-img" src={f.imgUrl} alt="" />
+                                    <span>{f.name}</span>
+                                </div>
+                            </Link>
                         )
                     })}
                 </div>
--- src/components/Fentity.js
+++ src/components/Fentity.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/fentity.css'
 
 
 class Fentity extends Component {
+    
     render() {
+        const fentitiesCat = this.props.match.params.fentities
+        const fentitiesName = this.props.match.params.name
+        const fentity = this.props.state[fentitiesCat].find(f => f.name === fentitiesName)
+        console.log(this.props);
         return (
             <div id="creature-container">
+                <Link to={`../${fentitiesCat}`}>Back</Link>
                 <h1>{fentity.name}</h1>
                 <img src={fentity.imgUrl} alt=""/>
                 <div className="title">Power:</div>
